5fe0f0a21b002025a37757a2df4009f96f23c97e
palla <palla02323@gmail.com>
2026-03-25 07:22:43 +0530

    first commit

---
+++ src/main/java/com/ecom/UserMsAwsCICD.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsAwsCICD {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsAwsCICD.class, args);
+	}
+
+}
---
+++ src/main/java/com/ecom/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ecom.repository;
+
+import com.ecom.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
